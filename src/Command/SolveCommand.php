5d0dea9fe9f1ec53b482fdc7ecf96a600b5e5637
Christian Flothmann <christian.flothmann@xabbuh.de>
2014-04-19 21:02:35 +0200

    remove unnecessary use statement

--- src/Command/SolveCommand.php
+++ src/Command/SolveCommand.php
@@ -17,7 +17,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Xabbuh\BRP\Algorithm\AlgorithmFactory;
-use Xabbuh\BRP\Algorithm\LaAlgorithm;
 use Xabbuh\BRP\Configuration\Loader\LoaderFactory;
 use Xabbuh\BRP\Solution\Dumper\ConsoleDumper;
 
